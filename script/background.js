45981cd720c9e84d4b1aaeabab5085813f7a5a6b
alexandre <alexandrebeauverger@gmail.com>
2017-06-03 19:36:39 +0200

    commentaire de la fonction parralax

--- script/background.js
+++ script/background.js
@@ -4,9 +4,9 @@ function parallax() {
 	/*calcul de la position du background selon le scrolling de la page*/
 	var yPos = window.pageYOffset / $scroll.dataset.speed;
 	yPos = -yPos;
-	
+	/*mise en place du background-position*/
 	var coords = '0% '+ yPos + 'px';
-	
+	/*modification du css*/
 	$scroll.style.backgroundPosition = coords;
 }
 
